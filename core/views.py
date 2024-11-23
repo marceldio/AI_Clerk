b0203162aa0a389be4d9acff0250c3041709fdaa
md <marceldiomare@gmail.com>
2024-11-23 23:28:08 +0300

    0002 added core/views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, FormParser
+from .utils import extract_text_from_image
 
-# Create your views here.
+class OCRAPIView(APIView):
+    parser_classes = (MultiPartParser, FormParser)
+
+    def post(self, request, *args, **kwargs):
+        """
+        Загружает изображение и возвращает распознанный текст.
+        """
+        file_obj = request.FILES.get('image')
+        if not file_obj:
+            return Response({"error": "Не загружено изображение"}, status=400)
+
+        # Сохраняем файл временно
+        text = extract_text_from_image(file_obj)
+        return Response({"text": text})
+
